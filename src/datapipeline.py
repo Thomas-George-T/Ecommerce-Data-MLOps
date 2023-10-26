cb24e2bf990ab1ef2e91a728c6b8130f2d2b533b
Thomas-George-T <tgt555@gmail.com>
2023-10-25 23:59:28 -0400

    pylint: added init-hook, started the datapipeline, adding zip folder for testing

--- src/datapipeline.py
+++ src/datapipeline.py
@@ -1,10 +1,11 @@
 """
 Modularized Data pipeline to form DAGs in the future
 """
-from .download_data import ingest_data
-from .unzip_data import unzip_file
+from download_data import ingest_data
+from unzip_data import unzip_file
 
 
 if __name__ == "__main__":
-    ZIPFILE_PATH = ingest_data("https://archive.ics.uci.edu/static/public/352/online+retail.zip")
-    UNZIPPED_FILE = unzip_file(ZIPFILE_PATH, 'data/')
+    ZIPFILE_PATH = ingest_data(
+        """https://archive.ics.uci.edu/static/public/352/online+retail.zip""")
+    UNZIPPED_FILE = unzip_file(ZIPFILE_PATH, 'data')
