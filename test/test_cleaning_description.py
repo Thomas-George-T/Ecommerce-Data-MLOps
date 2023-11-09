8518a04fd1cfe84e26b11144dc1e1dfc0aa8ff22
bardiamm <mouhebat.b@northeastern.edu>
2023-11-08 23:33:45 -0500

    updates_to_cleanining_description

--- test/test_cleaning_description.py
+++ test/test_cleaning_description.py
@@ -1,5 +1,31 @@
-import cleaning_description
-import pytest
+"""
+A test for checking cleaning description
+"""
 
-def test_cleaning_description(imput_data_frame):
-    
+import os
+import pickle
+import pytest
+from src.duplicates_handler import remove_duplicates
+
+# Determine the absolute path of the project directory
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+INPUT_PICKLE_PATH = os.path.join(PROJECT_DIR, 'data',
+                                 'processed','after_missing_values.pkl')
+OUTPUT_PICKLE_PATH = os.path.join(PROJECT_DIR, 'data',
+                                  'processed', 'after_duplicates.pkl')
+
+
+def test_cleaning_description():
+    """
+    Test to check if correct descriptions have been removed
+    """
+    result = cleaning_description(input_pickle_path=INPUT_PICKLE_PATH,
+                                    output_pickle_path=OUTPUT_PICKLE_PATH)
+
+    assert result == OUTPUT_PICKLE_PATH                                
+
+    with open(OUTPUT_PICKLE_PATH, "rb") as file:
+        df = pickle.load(file)        
+
+    #checking if correct descriptions have been removed       
+    assert len(df['Description'].isin(service_related_descriptions)) == 0                 
